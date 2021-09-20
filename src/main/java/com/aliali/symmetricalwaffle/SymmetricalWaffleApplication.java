3a2ab2bfb7c62bf55cd63743924055eff429814a
13circle <13circle97@gmail.com>
2021-09-20 09:20:19 +0900

    init spring boot project

---
+++ src/main/java/com/aliali/symmetricalwaffle/SymmetricalWaffleApplication.java
@@ -0,0 +1,13 @@
+package com.aliali.symmetricalwaffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymmetricalWaffleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymmetricalWaffleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aliali/symmetricalwaffle/SymmetricalWaffleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliali.symmetricalwaffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymmetricalWaffleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
